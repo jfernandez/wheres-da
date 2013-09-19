8538cce0afa6f5df823ad32cc5f17dcdcc182297
McDirty <wiredmilk@gmail.com>
2013-09-19 13:32:38 -0700

    updating files

--- Assets/start.js
+++ Assets/start.js
@@ -1,12 +1,25 @@
 ﻿#pragma strict
+var NarrativeTile1;
+var scrollSpeed : float = .5;
+var offset : float;
+var rotate : float;
 
 function Start () {
-
+	NarrativeTile1 = Resources.Load("Reunion");
 }
 
 function Update () {
 if(Input.GetMouseButton(0))
-    {
-        Application.LoadLevel("MainScene");
-    }
+    loadNext();
+}
+
+function loadNext() {	
+        //renderer.material.SetTextureScale = Vector2(100, 200);
+        renderer.material.mainTexture = NarrativeTile1;
+
+	    var scrollSpeed = 0.1;
+		
+		var offset = Time.time * scrollSpeed;
+	
+		renderer.material.mainTextureOffset = Vector2 (0,offset);	
 }
