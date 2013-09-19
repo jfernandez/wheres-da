f69914b5159fe8eaf51d9a469d53e1241e2ef639
amccurdy <amccurdy@newrelic.com>
2013-09-18 18:39:39 -0700

    initial

---
+++ Assets/tensionController.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+private var tensionPoints : float = 0;
+private var tensionStep : float  = 1;
+private var maxTension : float = 60;
+private var timerInterval : float = 1;
+private var timer : float;
+
+function Start () {
+  timer = timerInterval;
+}
+
+function Update () {
+  timer -= Time.deltaTime;
+
+  if(timer < 0) {
+    tensionPoints += tensionStep;
+    timer = timerInterval;
+    Debug.Log(tensionPoints);       
+  }
+
+  
+
+  if(tensionPoints >= maxTension) {
+    Debug.Log("Game Over!");
+  }
+}
+
+function OnGUI () {
+	var r : Rect = camera.pixelRect;
+		print ("Camera displays from " + r.yMin + " to " + r.yMax + " pixel");
+		
+	if (GUI.Button (Rect (r.xMax/2-200, r.yMax-400 ,400,200), "Tantrometer")) {
+		print ("You clicked the button!");
+	}
+}
