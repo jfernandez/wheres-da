cf56daee156968dbda7dca8c8c03b533e0721c01
McDirty <wiredmilk@gmail.com>
2013-09-21 20:28:32 -0700

    fixed scrolling

--- Assets/credits.js
+++ Assets/credits.js
@@ -2,18 +2,18 @@
 private var NarrativeTile1;
 private var scrollSpeed : float = .5;
 private var offset : float;
-private var rotate : float;
-private var clicked : boolean;
 private var textures = ["Reunion", "EndCredits"];
 private var x : int;
 private var switched : boolean;
+private var offsetDirection : float;
 
 function Start () {
 	NarrativeTile1 = Resources.Load("Reunion");	
+	renderer.material.mainTextureOffset = Vector2 (0,0);
 	x = 0;
 	switched = false;
 	scrollIt();
-	
+	offset = 0;
 }
 
 function Update () {
@@ -25,24 +25,35 @@ function scrollIt() {
 		//Debug.Log("Trying to scroll");
 		renderer.material.SetTextureScale("_MainTex", Vector2(1, .5));
 	    renderer.material.mainTexture = NarrativeTile1;
-	    
 		
 		//var offset = Time.time * scrollSpeed;	
-		var offset = -0.01;
-		while(offset < 0.5){//specific reunion offset
-			renderer.material.mainTextureOffset = Vector2 (0,offset);
-			offset = offset + 0.001;
-			//Debug.Log(offset);
-			yield WaitForSeconds(0.001);
-			
-			if(offset >= 0.49 && switched == false){
-		
-				//Fade? 
-				yield WaitForSeconds(2);
-				changeTexture();
-			} 
-		}	
-	//}
+		if(switched == false){
+		    offsetDirection = 0.002;
+		    while(offset < 0.5){//specific reunion offset
+				renderer.material.mainTextureOffset = Vector2 (0,offset);
+				offset = offset + offsetDirection;
+				
+				yield WaitForSeconds(0.01);
+			    Debug.Log(offset);
+				if(offset >= .5){
+					//Fade? 
+					yield WaitForSeconds(2);
+					changeTexture();
+					} 
+				}
+			}
+		else
+		{
+			if(switched == true){
+			    offsetDirection = -0.001;
+			    while(offset >= 0.5){
+					renderer.material.mainTextureOffset = Vector2 (0,offset);
+					offset = offset + offsetDirection;				
+					yield WaitForSeconds(0.01);
+					}
+					
+				}
+			}
 }
 
 function changeTexture() {
@@ -50,6 +61,7 @@ function changeTexture() {
 	switched = true;
 	NarrativeTile1 = Resources.Load("EndCredits");
 	renderer.material.mainTexture = NarrativeTile1;
+	renderer.material.mainTextureOffset = Vector2 (0,-.5);
 	//renderer.material.SetTextureScale("_MainTex", Vector2(1, 1));
 	scrollIt();
 }
